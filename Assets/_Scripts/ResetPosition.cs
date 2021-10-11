0150b3da76d9917175d9cb77aa9526715ce38148
Lea Lazer <lea.schorling@gmail.com>
2021-10-11 11:27:35 +0200

    add project data

---
+++ Assets/_Scripts/ResetPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    private Vector3 StartPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ResetToStartPosition()
+    {
+        transform.position = StartPosition;
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
---
+++ Assets/_Scripts/ResetRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetRotation : MonoBehaviour
+{
+    private Quaternion StartRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ResetToStartRotation()
+    {
+        transform.rotation = StartRotation;
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
